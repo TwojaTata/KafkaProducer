5a22bd255c9ed7b939f235785519ed7a0a4da7e9
Patryk Kucharski <Patryk_Kucharski@epam.com>
2019-08-20 11:37:34 +0200

    Add Producer.class

--- src/main/java/com/patrykkuchar/app/Main.java
+++ src/main/java/com/patrykkuchar/app/Main.java
@@ -1,7 +1,34 @@
-﻿package com.patrykkuchar.app;
+package com.patrykkuchar.app;
 
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
+import java.util.Properties;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Properties properties_0 = new Properties();
+    properties_0.put("bootstrap.servers", "localhost:9090");
+    properties_0.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_0.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_0.put("retries", "10");
+
+    Properties properties_1 = new Properties();
+    properties_1.put("bootstrap.servers", "localhost:9091");
+    properties_1.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_1.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_1.put("retries", "10");
+
+    Properties properties_2 = new Properties();
+    properties_2.put("bootstrap.servers", "localhost:9093");
+    properties_2.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_2.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    properties_2.put("retries", "10");
+
+    Thread producer_0 = new Thread(new Producer(properties_0), "producer_0");
+    Thread producer_1 = new Thread(new Producer(properties_1), "producer_1");
+    Thread producer_2 = new Thread(new Producer(properties_2), "producer_2");
+
+    producer_0.start();
+    producer_1.start();
+    producer_2.start();
+  }
 }
---
+++ src/main/java/com/patrykkuchar/app/Producer.java
@@ -0,0 +1,28 @@
+package com.patrykkuchar.app;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer implements Runnable {
+
+  private final Properties properties;
+
+  public Producer(Properties properties) {
+    this.properties = properties;
+  }
+
+  @Override
+  public void run() {
+    try (KafkaProducer kafkaProducer = new KafkaProducer(properties)) {
+      for (int i = 0; i < 30; i++) {
+        System.out.println("Thread " + Thread.currentThread().getName() + " sending message no " + i);
+        kafkaProducer.send(new ProducerRecord("topic", Integer.toString(i), "test message - " + i));
+      }
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+    }
+    System.out.println("Messages sent");
+  }
+}
